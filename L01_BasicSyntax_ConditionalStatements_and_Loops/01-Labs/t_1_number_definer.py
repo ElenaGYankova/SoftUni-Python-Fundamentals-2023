82148bd592d1699b95837207d5e3580046be6fad
Elena Yankova <122468758+ElenaGYankova@users.noreply.github.com>
2023-05-16 19:59:06 +0300

    Update t_1_number_definer.py
    Don't need those descriptions

--- L01_BasicSyntax_ConditionalStatements_and_Loops/01-Labs/t_1_number_definer.py
+++ L01_BasicSyntax_ConditionalStatements_and_Loops/01-Labs/t_1_number_definer.py
@@ -36,9 +36,3 @@ else:
     else:
         print("negative")
 '''
-
-###   Task Description   ### 01. Number Definer
-# Write a program that reads a floating-point number and:
-# -	prints "zero" if the number is zero
-# -	prints "positive" or "negative"
-# -	adds "small" if the absolute value of the number is less than 1 and it is not 0, or "large" if it exceeds 1 000 000
