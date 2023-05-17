a5695e9bbc828fd979a6070bf637f2fd826a4588
Elena Yankova <122468758+ElenaGYankova@users.noreply.github.com>
2023-05-17 12:56:17 +0300

    Update t_5_numbers_filter.py

--- L03_Lists_Basics/03-Labs/t_5_numbers_filter.py
+++ L03_Lists_Basics/03-Labs/t_5_numbers_filter.py
@@ -26,6 +26,40 @@ elif command == "positive":
 
 print(filtered_list)
 
+
+'''
+numbers = int(input())
+
+even = []
+odd = []
+positive = []
+negative = []
+
+for _ in range(numbers):
+    current_number = int(input())
+
+    if current_number % 2 == 0:
+        even.append(current_number)
+    else:
+        odd.append(current_number)
+
+    if current_number >= 0:
+        positive.append(current_number)
+    else:
+        negative.append(current_number)
+
+command = input()
+
+if command == "even":
+    print(even)
+elif command == "odd":
+    print(odd)
+elif command == "positive":
+    print(positive)
+elif command == "negative":
+    print(negative)
+'''
+
 '''
 lines = int(input())
 
