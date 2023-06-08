c4b44623cf1b432392793368359e334e31f56d0b
Elena Yankova <122468758+ElenaGYankova@users.noreply.github.com>
2023-06-08 16:19:13 +0300

    Add files via upload

---
+++ L04_Functions/04-Exercise/t_2_add_and_subtract.py
@@ -0,0 +1,30 @@
+def add_subtract(n1, n2, n3):
+    final_num = n1 + n2 - n3
+    return final_num
+
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+print(add_subtract(num1, num2, num3))
+
+
+# def sum_numbers(number01, number02):
+#     return number01 + number02
+#
+#
+# def subtract(sum, number03):
+#     return sum - number03
+#
+#
+# def add_and_subtract(number01, number02, number03):
+#     sum_of_numbers = sum_numbers(number01, number02)
+#     return subtract(sum_of_numbers, number03)
+#
+#
+# first_number = int(input())
+# second_number = int(input())
+# third_number = int(input())
+# print(add_and_subtract(first_number, second_number, third_number))
+
