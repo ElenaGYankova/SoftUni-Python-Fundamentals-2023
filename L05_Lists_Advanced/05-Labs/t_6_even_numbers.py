bb350c5c9952d30e853631d13a4b61b5783fd322
Elena Yankova <122468758+ElenaGYankova@users.noreply.github.com>
2023-06-10 16:17:53 +0300

    Update t_6_even_numbers.py

--- L05_Lists_Advanced/05-Labs/t_6_even_numbers.py
+++ L05_Lists_Advanced/05-Labs/t_6_even_numbers.py
@@ -8,6 +8,7 @@ print([index for index in range(len(numbers)) if numbers[index] % 2 == 0])
 # print(list(map(lambda x: numbers.index(x), list(filter(lambda x: x % 2 == 0, numbers)))))
 
 # numbers_string = input().split(", ")
+#
 # numbers = list(map(int, numbers_string))
 # indices = []
 # for index in range(len(numbers)):
