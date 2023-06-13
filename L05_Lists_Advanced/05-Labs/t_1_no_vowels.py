8c56a1589d2b66152a10446c254b7268aebff5ce
Elena Yankova <122468758+ElenaGYankova@users.noreply.github.com>
2023-06-13 08:23:35 +0300

    Update t_1_no_vowels.py

--- L05_Lists_Advanced/05-Labs/t_1_no_vowels.py
+++ L05_Lists_Advanced/05-Labs/t_1_no_vowels.py
@@ -3,6 +3,7 @@ vowels = ["a", "o", "e", "i", "u"]
 no_vowels = [letter for letter in text if letter.lower() not in vowels]
 print("".join(no_vowels))
 
+
 # vowels = ['a', 'o', 'u', 'e', 'i']
 # phrase = [character for character in input()]
 # new_phrase = [i for i in phrase if i.lower() not in vowels]
