a8696ab706ff01b4d0856fe941e9a68c26751ec4
Elena Yankova <122468758+ElenaGYankova@users.noreply.github.com>
2023-07-27 10:13:11 +0300

    Add files via upload

---
+++ L09_Dictionaries/09-Lab/t_5_ascii_values.py
@@ -0,0 +1,14 @@
+words = input().split(", ")
+ascii_dict = {word: ord(word) for word in words}
+print(ascii_dict)
+
+'''
+list_of_characters = input().split(", ")
+
+letter_dic = {}
+
+for letter in list_of_characters:
+    letter_dic[letter] = ord(letter)
+
+print(letter_dic)
+'''
